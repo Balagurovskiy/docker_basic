a68ce858c6607b46d55858204cffcc89ed237966
Balagurovskiy <38509010+Balagurovskiy@users.noreply.github.com>
2018-09-23 17:57:12 +0300

    Add files via upload

---
+++ JavaSample.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+ *  Compilation:  javac JavaSample.java
+ *  Execution:    java JavaSample
+ *
+ *  Prints "JavaSample". By tradition, this is everyone's first program.
+ *
+ ******************************************************************************/
+
+public class JavaSample {
+
+    public static void main(String[] args) {
+    	for (int i = 0; i < 10; i++)
+    		System.out.print(" *");
+        System.out.println("\t\tHello !");
+        System.out.println("\tThis is output from JavaSample.class");
+        for (int i = 0; i < 10; i++)
+    		System.out.print(" *");
+    }
+
+}
