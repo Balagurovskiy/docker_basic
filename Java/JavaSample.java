8fae505d380b1a9d5cd646fe16ae811f9f3f3323
Balagurovskiy <38509010+Balagurovskiy@users.noreply.github.com>
2018-09-25 20:20:33 +0300

    Update JavaSample.java

--- Java/JavaSample.java
+++ Java/JavaSample.java
@@ -9,23 +9,23 @@ public class JavaSample {
 	private static void printStars(int size) {
 		System.out.println("\n");
 		for (int i = 0; i < size; i++)
-    		System.out.print(" *");
-    	System.out.println("\n");
+		System.out.print(" *");
+		System.out.println("\n");
 	}
 
 	private static void printDate() {
 		System.out.println("\n");
 		java.util.Date date = new java.util.Date();
-	    System.out.println("\t" + date);
+		 System.out.println("\t" + date);
 		System.out.println("\n");
 	}
     
-    public static void main(String[] args) {
-    	printStars(25);    	
-        System.out.println("\t\t\tHello !");
-        printDate();
-        System.out.println("\tThis is output from JavaSample.class");
-        printStars(25); 
-    }
+	public static void main(String[] args) {
+		printStars(25);    	
+		System.out.println("\t\t\tHello !");
+		printDate();
+		System.out.println("\tThis is output from JavaSample.class");
+		printStars(25); 
+	}
 
 }
